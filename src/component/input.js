13a4ca25ccc645a2f3f615abb1e3be440538f42d
umakant <umakantsonwani@gmail.com>
2019-01-15 11:26:02 +0530

    Added Normal State Addition

--- src/component/input.js
+++ src/component/input.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class Input extends Component {
     constructor(props){
         super(props);
-        
+
+
         this.state = { 
             value1: '',
             value2: '',
             result: '' 
         };
+
+        const valuesall = {
+            dates: 'vca',
+            name: 'Umakant',
+            email: 'umakantsonwani@gmail.com'
+        }
 
         //this.state = { value2: '' };
         this.handleChange1 = this.handleChange1.bind(this);
@@ -18,13 +25,11 @@ class Input extends Component {
 
 
     handleChange1=(e)=>{
-        console.log(this.refs.value1.value);
-        this.setState({ value1 : this.refs.value1.value });
+        this.setState({ value1 : e.target.value });
     }
 
     handleChange2=(e)=>{
-        console.log(typeof(this.refs.value2.value));
-        this.setState({ value2 : this.refs.value1.value });
+        this.setState({ value2 : e.target.value });
     }
 
     addvalues=(e)=> {
@@ -38,11 +43,11 @@ class Input extends Component {
             <div className="test">
                 <label>
                     Value 1:
-                    <input type="text" ref="value1" onChange={this.handleChange1} />
+                    <input type="text" value={this.state.value1} onChange={this.handleChange1} />
                 </label>
                 <label>
                     Value 2:
-                    <input type="text" ref="value2" onChange={this.handleChange2} />
+                    <input type="text" value={this.state.value2} onChange={this.handleChange2} />
                 </label>
                 <label>
                     <input type="button" value="Submit" onClick={this.addvalues} />
